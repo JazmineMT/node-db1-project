713771ec43aadd901833267cf08192a227903302
JazmineMT <jazmineamcginnis@gmail.com>
2020-08-06 19:26:18 -0700

    added some things

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const server = require("./api/server.js");
 
 const PORT = process.env.PORT || 5000;
 
+server.get('/', (req, res)=> {
+  res.send('Working!')
+})
+
 server.listen(PORT, () => {
   console.log(`\n== API running on port ${PORT} ==\n`);
 });
--- queries.sql
+++ queries.sql
@@ -32,7 +32,17 @@ Country = 'Brazil' ;
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
+INSERT into Customers  (CustomerName , ContactName , Address, City, PostalCode,Country)
+values("The Shire" , "Bilbo Baggins", "1 Hobbit-Hole" , "Bag End" , "111" , "Middle Earth")
+;
+
+
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
+
+UPDATE customers 
+set PostalCode = "11122"
+where ContactName = "Bilbo Baggins"
+;
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
